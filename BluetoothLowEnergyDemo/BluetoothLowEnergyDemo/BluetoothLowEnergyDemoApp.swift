96784f39e535253205e4a81c7cca6be83abfb450
ShoheiTakino <shohei0522penguin@gmail.com>
2026-04-27 22:03:17 +0900

    init

---
+++ BluetoothLowEnergyDemo/BluetoothLowEnergyDemo/BluetoothLowEnergyDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BluetoothLowEnergyDemoApp.swift
+//  BluetoothLowEnergyDemo
+//
+//  Created by Shohei Takino on 2026/04/27.
+//
+
+import SwiftUI
+
+@main
+struct BluetoothLowEnergyDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
